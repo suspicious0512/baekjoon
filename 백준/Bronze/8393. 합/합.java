c217ccbc00b332771b4f7bd189a80c4eac7e22d7
최용준 <137700278+suspicious0512@users.noreply.github.com>
2023-06-29 22:44:55 +0900

    [Bronze V] Title: 합, Time: 220 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int count = 0;
+		for(int i=1;i<=n;i++)
+		{
+			count+=i;
+		}
+		
+		System.out.println(count);
+		
+		
+		
+		
+		
+		
+	}
+}
