0aa18653d2d9a35598cdfcc6943a1f48aeca2b72
최용준 <137700278+suspicious0512@users.noreply.github.com>
2023-06-29 22:45:28 +0900

    [Bronze V] Title: A+B - 8, Time: 296 ms, Memory: 18484 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int howmany = sc.nextInt();
+		int a =0;
+		int b =0;
+		for(int i = 0;i<howmany; i++) {
+			a =sc.nextInt();
+			b =sc.nextInt();
+			System.out.printf("Case #%d: %d + %d = %d\n",i+1,a,b,a+b);
+		}
+		
+	}
+}
