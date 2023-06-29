37e486933ecbb4cc7b6ce459cb1d1f988da76ed9
최용준 <137700278+suspicious0512@users.noreply.github.com>
2023-06-29 22:42:33 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		String siteid = sc.next();
+
+		System.out.println(siteid+"??!");
+	
+		
+		
+	}
+
+}
