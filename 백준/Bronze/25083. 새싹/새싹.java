f8f16aff6f0ab44bee756512e17ee9a323057224
최용준 <137700278+suspicious0512@users.noreply.github.com>
2023-06-29 22:49:06 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 13992 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+
+public class Main 
+{
+	
+	public static void main(String[] args) throws IOException
+	{
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("         ,r'\"7");
+		sb.append("\n");
+		sb.append("r`-_   ,'  ,/");
+		sb.append("\n");
+		sb.append(" \\. \". L_r'");
+		sb.append("\n");
+		sb.append("   `~\\/");
+		sb.append("\n");
+		sb.append("      |");
+		sb.append("\n");
+		sb.append("      |");
+		sb.append("\n");
+
+		System.out.println(sb);
+	}
+
+}
