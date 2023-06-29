bed65a01a80bd3e48a2294129b860fc03093f5cc
최용준 <137700278+suspicious0512@users.noreply.github.com>
2023-06-29 22:42:18 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextInt();
+		double b= sc.nextInt();
+		
+		double ans = a/b;
+		
+		System.out.println(ans);
+		
+	}
+
+}
